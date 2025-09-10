79bf019c20aba445f4466649d654feed361aea86
127MSafran <127msafran@frhsd.com>
2025-09-10 09:19:32 -0400

    Add comment for currencies needed

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+//Currencies: US to Euros, British Pounds, Rupees, Mexican Pesos, and Canadian dollars
+
 public class CurrencyConverter {
     public static void main(String[] args) {
         //temp
