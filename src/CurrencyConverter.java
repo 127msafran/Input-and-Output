f33eac12ee973398c0b095b2e61a5bedbf49e5f0
127RSire <127rsire@frhsd.com>
2025-09-10 09:23:44 -0400

    hello pliggie

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -6,10 +6,15 @@ public class CurrencyConverter {
     int USDollars;
     int Euros;
     int BritishPound;
-    int Rupies;
+    int Rupees;
     int MexicanPesos;
     int CanadianDollars
     public static void main(String[] args) {
         //temp
+        //1 INR = 0.011 USD
+        //1 Mexican Peso = 0.054 USD
+        //1 Euro = 1.17 USD
+        //1 Pound = 1.35 USD
+        //1 CAD = 0.72 USD
     }
 }
