f22bfccb660681776c8d43d3f5bf0416f84ea0a3
127MSafran <127msafran@frhsd.com>
2025-09-10 09:44:49 -0400

    Add text with input

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -10,12 +10,17 @@ public class CurrencyConverter {
     int MexicanPesos;
     int CanadianDollars;
     public static void main(String[] args) {
-        //temp
         //1 INR = 0.011 USD
         //1 Mexican Peso = 0.054 USD
         //1 Euro = 1.17 USD
         //1 Pound = 1.35 USD
         //1 CAD = 0.72 USD
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Currency Converter System!");
+        System.out.println("What currency do you have? (USD, EUR, GBP, INR, MXN, or CAD)");
+        String currencyInput = input.nextLine();
+        System.out.println("Please enter an amount for conversion: ");
+        double currencyAmount = input.nextDouble();
+        System.out.println("What currency would you like to convert to? (USD, EUR, GBP, INR, MXN, or CAD)");
     }
 }
