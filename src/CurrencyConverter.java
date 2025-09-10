4439f6bdec73a1c1a84a74b4002b29b3b777f7db
Pslusona <127Pmelo@frhsd.com>
2025-09-10 09:20:53 -0400

    Int for currencies

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class CurrencyConverter {
+    int USDollars;
+    int Euros;
+    int BritishPound;
+    int Rupies;
+    int MexicanPesos;
+    int CanadianDollars
     public static void main(String[] args) {
         //temp
     }
